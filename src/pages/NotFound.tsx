a2745056c59904ecd5471de7d4a37ff83ec58327
hnpf <deprecated@virex.lol>
2026-08-04 23:39:38 -0500

    remove useless ass bg linear gradient that never worked in the first place

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -89,7 +89,7 @@ const NotFound = ({ go }: { go?: (page: string) => void }) => {
           </motion.button>
         </motion.div>
       </div>
-      <div className="absolute inset-0 pointer-events-none opacity-[0.05] bg-[linear-gradient(rgba(18,16,16,0)_50%,rgba(0,0,0,0.25)_50%),linear-gradient(90deg,rgba(255,0,0,0.06),rgba(0,255,0,0.02),rgba(0,0,255,0.06))] bg-[length:100%_4px,4px_100%] z-20" />
+      <div className="absolute inset-0 pointer-events-none opacity-[0.05] z-20" />
     </div>
   );
 };
